7f33359927ffbaf3a4d7a7d463cdfffa1435984a
prolific-dev <de391ago@htwg-konstanz.de>
2022-02-15 14:16:15 +0100

    fix testing bug and CI not recognizing classes

--- src/test/scala/com.prolificdev/TestSpec.scala
+++ src/test/scala/com.prolificdev/TestSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.should.Matchers
 class TestSpec extends AnyWordSpec with Matchers{
   "A Test" when {
       "created" should {
-          val test = new Test
+          val test: Test = new Test()
 
           "have a test method" in {
               test.returnTest should be(1)
