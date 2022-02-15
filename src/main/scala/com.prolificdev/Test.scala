ba465496f80a46a29fec73679dcbf7b837be5e7d
prolific-dev <de391ago@htwg-konstanz.de>
2022-02-15 13:43:02 +0100

    adding test suit

---
+++ src/main/scala/com.prolificdev/Test.scala
@@ -0,0 +1,5 @@
+package `com.prolificdev`
+
+class Test {
+  def returnTest = 1
+}
---
+++ src/test/scala/com.prolificdev/TestSpec.scala
@@ -0,0 +1,16 @@
+package `com.prolificdev`
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+class TestSpec extends AnyWordSpec with Matchers{
+  "A Test" when {
+      "created" should {
+          val test = new Test
+
+          "have a test method" in {
+              test.returnTest should be(1)
+          }
+      }
+  }
+}
