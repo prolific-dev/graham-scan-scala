2228d7375167f3c23abfef545aad2596a5a2168b
prolific-dev <de391ago@htwg-konstanz.de>
2022-02-16 01:03:20 +0100

    Update Point.scala

--- src/main/scala/com/prolificdev/model/pointComponent/pointBasicImpl/Point.scala
+++ src/main/scala/com/prolificdev/model/pointComponent/pointBasicImpl/Point.scala
@@ -1,8 +1,9 @@
 package com.prolificdev.model.pointComponent.pointBasicImpl
 
 import com.prolificdev.model.pointComponent.PointInterface
+import com.google.inject.Inject
 
-case class Point(x: Int, y: Int) extends PointInterface {
+class Point @Inject (x: Int, y: Int) extends PointInterface {
 
     override def toString: String = "(" + x + " | " + y + ")"
 }
