afe8cd03697dfe07ca4125e7ba6f68eb87cee5e4
prolific-dev <de391ago@htwg-konstanz.de>
2022-02-16 01:12:16 +0100

    Setup module for DI

--- src/main/scala/com/prolificdev/GrahamScanModule.scala
+++ src/main/scala/com/prolificdev/GrahamScanModule.scala
@@ -1,3 +1,15 @@
 package com.prolificdev
 
+import com.google.inject.AbstractModule
+import com.prolificdev.model.pointComponent.PointInterface
+import com.prolificdev.model.pointComponent.pointBasicImpl.Point
+import com.prolificdev.model.fileIoComponent.FileIOInterface
+import com.prolificdev.model.fileIoComponent.FileIO
 
+class GrahamScanModule extends AbstractModule {
+
+    override def configure: Unit = {
+        bind(classOf[PointInterface]).to(classOf[Point])
+        bind(classOf[FileIOInterface]).to(classOf[FileIO])
+    }
+}
