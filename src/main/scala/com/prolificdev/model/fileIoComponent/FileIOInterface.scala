da377d844a882355fdcf139c7cc7d14d48f51881
prolific-dev <de391ago@htwg-konstanz.de>
2022-02-16 01:12:39 +0100

    Update FileIO

--- src/main/scala/com/prolificdev/model/fileIoComponent/FileIOInterface.scala
+++ src/main/scala/com/prolificdev/model/fileIoComponent/FileIOInterface.scala
@@ -1,5 +1,7 @@
 package com.prolificdev.model.fileIoComponent
 
 trait FileIOInterface {
-  
+    def load: Unit
+
+    def save: Unit
 }
--- src/main/scala/com/prolificdev/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
+++ src/main/scala/com/prolificdev/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
@@ -2,6 +2,10 @@ package com.prolificdev.model.fileIoComponent
 
 import com.google.inject.Inject
 
-class FileIO @Inject {
+class FileIO @Inject extends FileIOInterface {
+
+    override def load: Unit = ???
+
+    override def save: Unit = ???
   
 }
