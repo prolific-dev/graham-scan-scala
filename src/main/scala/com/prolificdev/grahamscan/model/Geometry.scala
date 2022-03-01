50df649a086c61c4045de156f59127972e19f455
prolific-dev <de391ago@htwg-konstanz.de>
2022-03-01 21:26:43 +0100

    Added distance calculation between two points

---
+++ src/main/scala/com/prolificdev/grahamscan/model/Geometry.scala
@@ -0,0 +1,14 @@
+package com.prolificdev.grahamscan.model
+
+import com.prolificdev.grahamscan.model.Point
+
+import scala.math.*
+
+class Geometry {
+  def angle(p1: Point, p2: Point): Double =
+    val theta = atan2(p1.y - p2.y, p1.x - p2.x) + math.Pi / 2.0
+    val angle = toDegrees(theta)
+    if (angle < 0) angle + 360 else angle
+
+  def distance(p1: Point, p2: Point): Double = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2))
+}
