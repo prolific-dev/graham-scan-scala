202ed0bdcb7456435346b2d688a4defdca480fbb
prolific-dev <de391ago@htwg-konstanz.de>
2022-03-01 21:49:36 +0100

    Update project structure

--- src/main/scala/com/prolificdev/grahamscan/model/fileIoComponent/FileIOInterface.scala
+++
@@ -1,9 +0,0 @@
-package com.prolificdev.grahamscan.model.fileIoComponent
-
-import com.prolificdev.grahamscan.model.Point
-
-trait FileIOInterface {
-  def load: Seq[Point]
-
-  def save(points: Seq[Point]): Unit
-}
--- src/main/scala/com/prolificdev/grahamscan/model/geometryComponent/GeometryInterface.scala
+++
@@ -1,7 +0,0 @@
-package com.prolificdev.grahamscan.model.geometryComponent
-
-import com.prolificdev.grahamscan.model.Point
-
-trait GeometryInterface {
-  def angle(p1: Point, p2: Point): Double
-}
