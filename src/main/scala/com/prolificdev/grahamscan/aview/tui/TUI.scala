dfe7425747fc284b4a3c508de208b64c2f8ae4c0
prolific-dev <de391ago@htwg-konstanz.de>
2022-03-02 11:31:23 +0100

    Update TUI

--- src/main/scala/com/prolificdev/grahamscan/aview/tui/TUI.scala
+++ src/main/scala/com/prolificdev/grahamscan/aview/tui/TUI.scala
@@ -1,11 +1,35 @@
 package com.prolificdev.grahamscan.aview.tui
 
 import com.prolificdev.grahamscan.controller.controllerComponent.ControllerInterface
-import com.prolificdev.grahamscan.util.Observer
+import com.prolificdev.grahamscan.model.fileIoComponent.fileIoJsonImpl.FileIO
+import com.prolificdev.grahamscan.util.{Observer, Point}
+
+import scala.io.StdIn.readLine
 
 class TUI(controller: ControllerInterface) extends Observer {
   controller.add(this)
 
-  override def update: Unit = ???
+  def run: Unit =
+    println("Welcome to the Graham Scan Calculation Software")
+    inputLoop
+
+  def inputLoop: Unit =
+    readLine match {
+      case "q" =>
+      case input: String => processInputLine(input); inputLoop
+    }
+
+  def processInputLine(input: String): Unit =
+    input match {
+      case "s" => controller.save(new FileIO().load)
+      case "l" => controller.load
+    }
+
+  override def update: Unit =
+    println(controller.status.message)
+    println("\nInput Data: " + controller.calc.input.toString)
+    println("\nInner Data: " + controller.calc.inner.toString)
+    println("Hull Data" + controller.calc.hull.toString)
+
 
 }
--- src/test/scala/com/prolificdev/grahamscan/aview/tui/TUISpec.scala
+++ src/test/scala/com/prolificdev/grahamscan/aview/tui/TUISpec.scala
@@ -1,5 +1,19 @@
 package com.prolificdev.grahamscan.aview.tui
 
-class TUISpec {
+import com.google.inject.Guice
+import com.prolificdev.grahamscan.GrahamScanModule
+import com.prolificdev.grahamscan.controller.controllerComponent.controllerBasicImpl.Controller
+import com.prolificdev.grahamscan.model.calculateComponent.CalculateInterface
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
+class TUISpec extends AnyWordSpec with Matchers {
+  "A TUI" when {
+    val injector = Guice.createInjector(new GrahamScanModule)
+    val calc = injector.getInstance(classOf[CalculateInterface])
+    val controller = new Controller(calc)
+    "created" should {
+      val tui = new TUI(controller)
+    }
+  }
 }
