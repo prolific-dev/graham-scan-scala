5805bd2821edf5bb9103578c013ac505b8dd5704
prolific-dev <de391ago@htwg-konstanz.de>
2022-03-02 11:32:49 +0100

    Update ControllerInterface

--- src/main/scala/com/prolificdev/grahamscan/controller/controllerComponent/ControllerInterface.scala
+++ src/main/scala/com/prolificdev/grahamscan/controller/controllerComponent/ControllerInterface.scala
@@ -1,10 +1,12 @@
 package com.prolificdev.grahamscan.controller.controllerComponent
 
 import com.prolificdev.grahamscan.controller.Status
-import com.prolificdev.grahamscan.model.calculateComponent.calculateBasicImpl.Calculate
+import com.prolificdev.grahamscan.model.calculateComponent.CalculateInterface
 import com.prolificdev.grahamscan.util.{Observable, Point}
 
 trait ControllerInterface extends Observable {
+  def calc: CalculateInterface
+
   def status: Status
 
   def save(points: Vector[Point]): Unit
