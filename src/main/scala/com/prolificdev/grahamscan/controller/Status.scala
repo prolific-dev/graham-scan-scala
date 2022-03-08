d325d8ca1d64be73ac01ff5678b4e0a646f87f23
prolific-dev <de391ago@htwg-konstanz.de>
2022-03-08 18:41:01 +0100

    Improved Status enums

--- src/main/scala/com/prolificdev/grahamscan/controller/Status.scala
+++ src/main/scala/com/prolificdev/grahamscan/controller/Status.scala
@@ -6,5 +6,8 @@ enum Status(status: String):
   case IDLE extends Status("IDLE")
   case SAVED extends Status("DATA SAVED")
   case LOADED extends Status("DATA LOADED")
+  case CONVERTED extends Status("DATA CONVERTED")
+  case CHANGED extends Status("DATA CHANGED")
+  case CLEARED extends Status("DATA CLEARED")
 
 end Status
--- src/test/scala/com/prolificdev/grahamscan/controller/StatusSpec.scala
+++ src/test/scala/com/prolificdev/grahamscan/controller/StatusSpec.scala
@@ -9,11 +9,19 @@ class StatusSpec extends AnyWordSpec with Matchers {
       val idle = Status.IDLE
       val saved = Status.SAVED
       val loaded = Status.LOADED
+      val converted = Status.CONVERTED
+      val changed = Status.CHANGED
+      val cleared = Status.CLEARED
+
 
       "have a message" in {
         idle.message should be("IDLE")
         saved.message should be("DATA SAVED")
         loaded.message should be("DATA LOADED")
+        converted.message should be("DATA CONVERTED")
+        changed.message should be("DATA CHANGED")
+        cleared.message should be("DATA CLEARED")
+
       }
     }
   }
